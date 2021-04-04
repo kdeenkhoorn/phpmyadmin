cd5e61794b9495b074605d36fb42311be3e8a16a
Klaas Eenkhoorn <klaas@int.kdedesign.nl>
2021-04-04 23:00:31 +0200

    Upgraded to PHPMyadmin 5.1.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set requested phpMyAdmin version
-ADMINVER=5.0.2
+ADMINVER=5.1.0
 
 docker build -t kdedesign/phpmyadmin:latest ./build-context --build-arg ADMINVER=${ADMINVER}
 docker tag kdedesign/phpmyadmin:latest kdedesign/phpmyadmin:${ADMINVER}
